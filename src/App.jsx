24b0bce70d5e3c281b80b851bb8b8322c5f3918f
aarto27 <sanchitsnarwat@gmail.com>
2025-03-27 18:10:40 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,42 @@ import Login from "./Components/Auth/Login.jsx";
 import EmployeeDashboard from "./Components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./Components/Dashboard/AdminDashboard";
 import { setLocalStorage } from "./utils/LocalStorage";
-import {AuthProvider} from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   // useEffect(() => {
   //   setLocalStorage()
   // },)
+
   const [user, setUser] = useState(null);
   const [loggedInUser, setLoggedInUser] = useState(null);
 
   const handleLogin = (email, password) => {
-    if (data && data.admin.find((e)=>{
-       e.email == email && e.password == password
-    })) {
-      setUser("admin");
-      localStorage.setItem('loggedInUser',JSON.stringify({role:'admin'}))
-    } else if (data) {
-      const employee = data.employee.find((e)=>{e.email == email && e.password == password})
-      if(employee){
-      setUser("employee");
-      setLoggedInUser(employee)
-      localStorage.setItem('loggedInUser',JSON.stringify({role:'employee'}))
+    if (data) {
+      const admin = data.admin.find(
+        (e) => e.email === email && e.password === password
+      );
+      if (admin) {
+        setUser("admin");
+        localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin" }));
+        return;
+      }
+      const employee = data.employee.find(
+        (e) => e.email === email && e.password === password
+      );
+      if (employee) {
+        setUser("employee");
+        setLoggedInUser(employee);
+        localStorage.setItem(
+          "loggedInUser",
+          JSON.stringify({ role: "employee" })
+        );
+        return;
+      }
     }
   };
 
   const data = useContext(AuthProvider);
- 
 
   return (
     <>
@@ -42,7 +52,6 @@ function App() {
       )}
     </>
   );
-}
 }
 
 export default App;
